b7ed2cbcb709094038f93a8db368135bb2226dd3
futuresplayer1 <jpresiliencygroup@gmail.com>
2026-05-25 19:58:06 -0400

    Add dashboard, python package, and tests

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,24 +1,23 @@
 # Import the QueryBase class
-#### YOUR CODE HERE
+from employee_events.query_base import QueryBase
 
 # Import dependencies needed for sql execution
 # from the `sql_execution` module
-#### YOUR CODE HERE
+from employee_events.sql_execution import QueryMixin
 
 # Define a subclass of QueryBase
 # called Employee
-#### YOUR CODE HERE
+class Employee(QueryBase):
 
     # Set the class attribute `name`
     # to the string "employee"
-    #### YOUR CODE HERE
-
+    name = "employee"
 
     # Define a method called `names`
     # that receives no arguments
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
+    def names(self):
         
         # Query 3
         # Write an SQL query
@@ -27,14 +26,19 @@
         # 2. The employee's id
         # This query should return the data
         # for all employees in the database
-        #### YOUR CODE HERE
-    
+        sql_query = """
+            SELECT first_name || ' ' || last_name AS full_name,
+                   employee_id
+            FROM employee
+            ORDER BY full_name
+        """
+        return self.query(sql_query)
 
     # Define a method called `username`
     # that receives an `id` argument
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
+    def username(self, id):
         
         # Query 4
         # Write an SQL query
@@ -42,24 +46,27 @@
         # Use f-string formatting and a WHERE filter
         # to only return the full name of the employee
         # with an id equal to the id argument
-        #### YOUR CODE HERE
-
+        sql_query = f"""
+            SELECT first_name || ' ' || last_name AS full_name
+            FROM employee
+            WHERE employee_id = {id}
+        """
+        return self.query(sql_query)
 
     # Below is method with an SQL query
     # This SQL query generates the data needed for
     # the machine learning model.
     # Without editing the query, alter this method
     # so when it is called, a pandas dataframe
-    # is returns containing the execution of
+    # is returned containing the execution of
     # the sql query
-    #### YOUR CODE HERE
     def model_data(self, id):
 
-        return f"""
+        return self.pandas_query(f"""
                     SELECT SUM(positive_events) positive_events
                          , SUM(negative_events) negative_events
                     FROM {self.name}
                     JOIN employee_events
                         USING({self.name}_id)
                     WHERE {self.name}.{self.name}_id = {id}
-                """
+                """)
--- python-package/employee_events/query_base.py
+++ python-package/employee_events/query_base.py
@@ -1,50 +1,31 @@
-# Import any dependencies needed to execute sql queries
-# YOUR CODE HERE
+from employee_events.sql_execution import QueryMixin
 
-# Define a class called QueryBase
-# Use inheritance to add methods
-# for querying the employee_events database.
-# YOUR CODE HERE
+class QueryBase(QueryMixin):
 
-    # Create a class attribute called `name`
-    # set the attribute to an empty string
-    # YOUR CODE HERE
+    name = ""
 
-    # Define a `names` method that receives
-    # no passed arguments
-    # YOUR CODE HERE
-        
-        # Return an empty list
-        # YOUR CODE HERE
+    def names(self):
+        return []
 
+    def event_counts(self, id):
+        sql_query = f"""
+            SELECT event_date,
+                   CAST(SUM(positive_events) AS INTEGER) AS positive_events,
+                   CAST(SUM(negative_events) AS INTEGER) AS negative_events
+            FROM employee_events
+            JOIN {self.name}
+                USING({self.name}_id)
+            WHERE {self.name}.{self.name}_id = {id}
+            GROUP BY event_date
+            ORDER BY event_date
+        """
+        return self.pandas_query(sql_query)
 
-    # Define an `event_counts` method
-    # that receives an `id` argument
-    # This method should return a pandas dataframe
-    # YOUR CODE HERE
-
-        # QUERY 1
-        # Write an SQL query that groups by `event_date`
-        # and sums the number of positive and negative events
-        # Use f-string formatting to set the FROM {table}
-        # to the `name` class attribute
-        # Use f-string formatting to set the name
-        # of id columns used for joining
-        # order by the event_date column
-        # YOUR CODE HERE
-            
-    
-
-    # Define a `notes` method that receives an id argument
-    # This function should return a pandas dataframe
-    # YOUR CODE HERE
-
-        # QUERY 2
-        # Write an SQL query that returns `note_date`, and `note`
-        # from the `notes` table
-        # Set the joined table names and id columns
-        # with f-string formatting
-        # so the query returns the notes
-        # for the table name in the `name` class attribute
-        # YOUR CODE HERE
-
+    def notes(self, id):
+        sql_query = f"""
+            SELECT note_date, note
+            FROM notes
+            WHERE {self.name}_id = {id}
+            ORDER BY note_date
+        """
+        return self.pandas_query(sql_query)
